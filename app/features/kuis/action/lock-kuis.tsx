53ce0e2435731fa9f88567cf9cb3ec1ad6017c5e
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-01-18 16:06:12 +0700

    fix switch and UI

--- app/features/kuis/action/lock-kuis.tsx
+++ app/features/kuis/action/lock-kuis.tsx
@@ -1,12 +1,26 @@
 import { dataWithSuccess, dataWithWarning } from "remix-toast";
 import { updateKuisMetaData } from "../services/updateKuisMetaData";
 import type { Route } from "./+types/lock-kuis";
+import { getJumlahQuestionsByIdKuis } from "../services/getJumlahQuestionsByIdKuis";
 
 export async function action({ request, params, context }: Route.ActionArgs) {
 
     const formData = await request.formData()
     const isLocked = formData.get("locked") === "true"
 
+    // jika jumlah soal 0, cegah kuis terpublish
+    const jumlahQuestions = await getJumlahQuestionsByIdKuis(params.idKuis)
+    if (jumlahQuestions === 0) {
+        await updateKuisMetaData(params.idKuis, {
+            isLocked: true,
+        })
+        return dataWithWarning({ isLocked: true }, {
+            message: "Belum ada soal",
+            description: "Kuis masih terkunci, tambahkan soal"
+        })
+    }
+
+    // update status kuis
     await updateKuisMetaData(params.idKuis, {
         isLocked: isLocked,
     })
