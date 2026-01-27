8697e47f1faacebc34df597fbefe4934f77672b4
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-01-28 00:28:57 +0700

    detail upload done

--- test/debug.ts
+++ test/debug.ts
@@ -1,9 +1,9 @@
-import { getSkillStats } from "@/features/dashboard/public/services/getSkillStats";
-import { getSkillByIdPicSubSkill } from "@/features/subskill/services/getSkillByIdPicSubSkill";
+
+import { getSkillProgressDetailByIdSkill } from "@/features/subskill/services/getSkillProgressDetailByIdSkill";
 
 
 async function main() {
-    const result = await getSkillByIdPicSubSkill("8a623469-4b42-4bf2-8bd9-7dc38ce10f5d");
+    const result = await getSkillProgressDetailByIdSkill("a9bce9a3-5d3f-4db7-9e41-9f8a6cfb92c1", "8a623469-4b42-4bf2-8bd9-7dc38ce10f5d");
 
     console.dir(result, { depth: null }); // biar nested kebuka semua
 }
