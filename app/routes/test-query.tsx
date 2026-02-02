fa175a16e8633a4b5eb2cc06648bfa27c8a61139
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-02-02 16:59:12 +0700

    test query api

--- app/routes/test-query.tsx
+++ app/routes/test-query.tsx
@@ -1,10 +1,19 @@
 import { db } from "database/connect";
 import type { Route } from "./+types/test-query";
-import { mSkill } from "database/schema";
+import { mSkill, mSubSkill, tDokumen, tStatusBaca } from "database/schema";
+import { and, eq, sql } from "drizzle-orm";
 
 export async function loader({ request, params, context }: Route.LoaderArgs) {
 
-    const res = await db.select().from(mSkill)
+    const t_statBaca = await db.select({
+        idPembaca: tStatusBaca.idPembaca,
+        jumlahDokumen: sql<number>`cast(count(${tStatusBaca.idDokumen}) as int)`,
+    }).from(mSubSkill)
+        .leftJoin(tDokumen, eq(tStatusBaca.idDokumen, tDokumen.idDokumen))
+        .leftJoin(mSubSkill, eq(tDokumen.idDokumen, mSubSkill.idDokumen))
+        .where(eq(tStatusBaca.idPembaca, "8a623469-4b42-4bf2-8bd9-7dc38ce10f5d"))
+        .groupBy(tStatusBaca.idPembaca)
+    // .as("t_statBaca");
 
-    return { res }
+    return { t_statBaca }
 }
