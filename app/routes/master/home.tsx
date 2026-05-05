29e6f544f881cd593947ab38609c739837422377
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-05-05 08:47:29 +0700

    prepare for dashboard PIC

--- app/routes/master/home.tsx
+++ app/routes/master/home.tsx
@@ -1,6 +1,13 @@
 import { HeaderRoute } from "@/components/header-route";
 import type { Route } from "./+types/home";
 
+export async function loader({ request, params, context }: Route.LoaderArgs) {
+
+    // data PIC yang telah upload dan membuat kuis
+
+    return {}
+}
+
 export default function MasterHome({ }: Route.ComponentProps) {
 
     return (
